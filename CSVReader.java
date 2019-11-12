f6662cae8114e58d13383ca73dfb37882f5cc121
Amaury Le Roux Dupeyron <amaurylrd@yahoo.fr>
2019-11-12 15:28:48 +0100

    CSVReader

---
+++ CSVReader.java
@@ -0,0 +1,36 @@
+package CSVUtil;
+
+import java.util.Scanner;
+import java.util.Map;
+import java.util.Arrays;
+
+import java.io.FileNotFoundException;
+
+public class CSVReader extends CSVEdit {
+    public Map<String, String[]> read() {
+        return read(TOKEN_SEPARATOR, SKIP_FIRST_LINE);
+    }
+
+    public Map<String, String[]> read(String separator, boolean skipFirstLine) {
+        Map<String, String[]> table = new java.util.Hashtable<String, String[]>();
+        if (workbook != null) {
+            try {
+                Scanner inputStream = new Scanner(workbook);
+                if (skipFirstLine)
+                    inputStream.nextLine();
+                while (inputStream.hasNext()) {
+                    String[] tuple = parseLine(inputStream.nextLine(), separator);
+                    table.put(tuple[0], Arrays.copyOfRange(tuple, 1, tuple.length));
+                }
+                inputStream.close();
+            } catch (FileNotFoundException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        return table;
+    }
+
+    private String[] parseLine(String line, String separator) {
+        return line.split(separator);
+    }
+}
